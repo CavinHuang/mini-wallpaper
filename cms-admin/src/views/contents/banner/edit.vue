ef86df58f849bc40c4a7ea97f2ba021249d121aa
CavinHuang <sujinw@qq.com>
2022-10-09 18:04:04 +0800

    feat: 增加轮播图编辑页面

--- cms-admin/src/views/contents/banner/edit.vue
+++ cms-admin/src/views/contents/banner/edit.vue
@@ -1,28 +1,162 @@
 <template>
   <div class="card">
-    <div class="banner-position" v-for="item in positons" :key="item.id" @click="navigator('/')">
-      <div class="left-content">
-        <div class="edit-text">编辑图片</div>
-        <div class="position-title">{{ item.position }}</div>
-      </div>
-      <div class="icon-image">
-        <ElIcon class="icon">
-          <Picture />
-        </ElIcon>
-      </div>
+    <div class="card-title">轮播图编辑</div>
+    <div class="card-subtitle">11</div>
+    <ElDivider />
+    <div class="edit-container">
+      <FormProvider :form="form">
+        <SchemaField :schema="schema" />
+        <p class="help-block">若要跳转页面，请选择对应的数据或填写跳转的 URL 地址，不跳转请填写 “#” 号占位。</p>
+        <ElDivider />
+        <Submit @submit="log">提交</Submit>
+      </FormProvider>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup name="banner">
-import { ElIcon } from 'element-plus'
+import { ElDivider } from 'element-plus'
 import { Picture } from '@element-plus/icons-vue'
 import { BannerApi } from '@/api/modules'
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+import { createForm } from '@formily/core'
+import { FormProvider, createSchemaField } from '@formily/vue'
+import { Submit, FormItem, Space, Input, Select, DatePicker, ArrayItems, Upload } from '@formily/element-plus'
 
 const router = useRouter()
 const navigator = (path: string) => router.push({ path })
+
+const { SchemaField } = createSchemaField({
+  components: {
+    FormItem,
+    Space,
+    Input,
+    Upload,
+    Select,
+    DatePicker,
+    ArrayItems
+  }
+})
+
+const form = createForm()
+const schema = {
+  type: 'object',
+  properties: {
+    array: {
+      type: 'array',
+      'x-component': 'ArrayItems',
+      'x-decorator': 'FormItem',
+      title: '',
+      items: {
+        type: 'object',
+        properties: {
+          space: {
+            type: 'void',
+            'x-component': 'Space',
+            properties: {
+              cover: {
+                type: 'array',
+                title: '',
+                'x-decorator': 'FormItem',
+                'x-component': 'Upload',
+                'x-component-props': {
+                  listType: 'picture-card',
+                  multiple: false,
+                  action: 'https://formily-vue.free.beeceptor.com/file'
+                },
+                required: true
+              },
+              space: {
+                type: 'void',
+                'x-component': 'Space',
+                'x-component-props': {
+                  style: {
+                    display: 'flex',
+                    flexDirection: 'column'
+                  }
+                },
+                properties: {
+                  input: {
+                    type: 'string',
+                    title: '输入框1',
+                    'x-decorator': 'FormItem',
+                    'x-component': 'Input'
+                  },
+                  input2: {
+                    type: 'string',
+                    title: '输入框2',
+                    'x-decorator': 'FormItem',
+                    'x-component': 'Input'
+                  }
+                }
+              },
+              space2: {
+                type: 'void',
+                'x-component': 'Space',
+                'x-component-props': {
+                  style: {
+                    display: 'flex',
+                    flexDirection: 'column'
+                  }
+                },
+                properties: {
+                  space: {
+                    type: 'void',
+                    'x-component': 'Space',
+                    properties: {
+                      up: {
+                        type: 'void',
+                        'x-decorator': 'FormItem',
+                        'x-component': 'ArrayItems.MoveUp'
+                      },
+                      down: {
+                        type: 'void',
+                        'x-decorator': 'FormItem',
+                        'x-component': 'ArrayItems.MoveDown'
+                      },
+                      remove: {
+                        type: 'void',
+                        'x-decorator': 'FormItem',
+                        'x-component': 'ArrayItems.Remove'
+                      }
+                    }
+                  },
+                  select: {
+                    type: 'string',
+                    title: '下拉框',
+                    enum: [
+                      { label: '选项1', value: 1 },
+                      { label: '选项2', value: 2 }
+                    ],
+                    'x-decorator': 'FormItem',
+                    'x-component': 'Select',
+                    'x-component-props': {
+                      style: {
+                        width: '250px'
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      },
+      properties: {
+        add: {
+          type: 'void',
+          title: '添加条目',
+          'x-component': 'ArrayItems.Addition'
+        }
+      }
+    }
+  }
+}
+
+const log = (values: any) => {
+  console.log(values)
+}
 
 const positons = ref<any[]>([])
 function getPositions() {
@@ -32,7 +166,6 @@ function getPositions() {
     }
   })
 }
-
 getPositions()
 </script>
 
