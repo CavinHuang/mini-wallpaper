5ca64c8dd1bb524fc3a29d11759de192d9d67819
CavinHuang <sujinw@qq.com>
2022-12-02 21:12:14 +0800

    feat: 创建文章相关的表

---
+++ api-server/ormconfig.ts
@@ -0,0 +1,23 @@
+import { DataSource } from 'typeorm'
+
+export default new DataSource({
+  "type": "mysql",
+  "host": "175.178.179.97",
+  "port": 33068,
+  "username": "mini-cms",
+  "password": "SZCYaKRekDreKLDt",
+  "database": "mini-cms",
+  "entities": [
+    "src/models/entitys/*.ts"
+  ],
+  "migrations": [
+    "src/models/migration/*.ts"
+  ],
+  "subscribers": [
+    "src/models/subscribers/*.ts"
+  ],
+  migrationsRun: false,
+  "synchronize": false,
+  "logging": true,
+  migrationsTableName: 'history'
+})
